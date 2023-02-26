b0f18ea2eac65e4574f879018c68ffd574076496
Polina Shliakhina <98726255+codedbypolina@users.noreply.github.com>
2023-02-25 21:36:49 -0500

    Adding structure of Navbar

---
+++ meme-generator/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import meme from "../images/troll.png"
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <img src={meme} className="meme" />
+            <h3>Meme Generator</h3>
+            <h2>React Course - Project 3</h2>
+        </div>
+    )
+}
