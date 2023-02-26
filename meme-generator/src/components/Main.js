27dd5c2d2f19a00902d614381e97832d50dbc719
Polina Shliakhina <98726255+codedbypolina@users.noreply.github.com>
2023-02-26 18:56:19 -0500

    Added main section

---
+++ meme-generator/src/components/Main.js
@@ -0,0 +1,12 @@
+export default function Main() {
+    return (
+        <div>
+            <div className="input">
+                <input type="text" className="first-input" />
+                <input type="text" className="second-input" />
+            </div>
+            <button>Create a new meme image🖼</button>
+            <div className="image-output">Image Output</div>
+        </div>
+    )
+}
--- meme-generator/src/components/Navbar.js
+++ meme-generator/src/components/Navbar.js
@@ -6,8 +6,6 @@ export default function Navbar() {
 
             <img src={meme} className="meme" />
             <h3>Meme Generator</h3>
-
-
             <h2>React Course - Project 3</h2>
         </div>
     )
