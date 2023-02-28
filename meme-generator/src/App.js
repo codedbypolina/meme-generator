1d34d4aee74dd5af4639a05bdfb1fa30490bd41e
Polina Shliakhina <98726255+codedbypolina@users.noreply.github.com>
2023-02-28 13:21:13 -0500

    Console log random meme from data

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
+import data from "../src/memeData"
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Main item={data} />
     </div>
   );
 }
--- meme-generator/src/components/Main.js
+++ meme-generator/src/components/Main.js
@@ -1,13 +1,19 @@
-export default function Main() {
+
+export default function Main(props) {
+    function showImage() {
+        let random = props.item.data.memes[Math.floor(Math.random() * 100)]
+        console.log(random)
+    }
     return (
         <div>
             <div className="input-section">
                 <input type="text" className="first-input" placeholder="Top text" />
                 <input type="text" className="second-input" placeholder="Bottom Text" />
             </div>
-            <div className="image-section"><button>Get a new meme image  🖼</button></div>
+            <div className="image-section" onClick={showImage}><button>Get a new meme image  🖼</button></div>
 
-            <div className="image-output"></div>
+            <div className="image-output">
+            </div>
         </div>
     )
 }
