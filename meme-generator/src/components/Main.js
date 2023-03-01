a4788d52a8da48d291b992aa3e3fe61e811d281b
Polina Shliakhina <98726255+codedbypolina@users.noreply.github.com>
2023-03-01 17:01:25 -0500

    Made images appear on click

--- meme-generator/src/components/Main.js
+++ meme-generator/src/components/Main.js
@@ -1,11 +1,25 @@
+/**
+     * Challenge: Save the random meme URL in state
+     * - Create new state called `memeImage` with an
+     *   empty string as default
+     * - When the getMemeImage function is called, update
+     *   the `memeImage` state to be the random chosen
+     *   image URL
+     * - Below the div.form, add an <img /> and set the
+     *   src to the new `memeImage` state you created
+     */
+import { useState } from "react"
 
 export default function Main(props) {
     function showImage() {
         let memeArray = props.item.data.memes
         let randomNumber = Math.floor(Math.random() * memeArray.length)
         let memeUrl = memeArray[randomNumber].url
-        console.log(memeUrl)
+        setMemeImage(memeUrl)
     }
+
+    const [memeImage, setMemeImage] = useState("")
+
     return (
         <div>
             <div className="input-section">
@@ -15,6 +29,7 @@ export default function Main(props) {
             <div className="image-section" onClick={showImage}><button>Get a new meme image  🖼</button></div>
 
             <div className="image-output">
+                <img src={memeImage} className="memeImage" />
             </div>
         </div>
     )
